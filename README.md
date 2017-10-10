cb2a0e224428ef630d018e743b25bbfe31972083
travis <t3patterson@gmail.com>
2017-10-09 23:09:21 -0500

    files

---
+++ README.md
@@ -0,0 +1,12 @@
+# JS - DOM Manipulations - Conditional Counter
+
+### The Demo
+![mockup](images/increase-decrease.gif)
+
+### Description
+Manipulate the DOM so that the counter can add one or subtract one from the number. The number should never exceed 10 or be less than 0. If the user pushes the -/+ beyond 10 or 0, then pop an alert box.
+
+**Hint**
+You may want to use Javascript's `parseInt` utility to convert a string to an integer.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
---
+++ js/app.js
@@ -0,0 +1,29 @@
+document
+  .querySelector('.btn-increment')
+  .addEventListener('click', function increaseCount(evt){
+    // YOUR CODE for INCREMENT BTN HERE
+
+
+  })
+
+
+document
+  .querySelector('.btn-subtract')
+  .addEventListener('click', function decreaseCount(evt){
+    // YOUR CODE for DECREMENT BTN HERE
+
+  })
+
+
+/* ---- ADVENTURE MODE ---- */
+/* Use only one Event Listener on the parent 
+    <div class="component"> element
+ */
+
+/* Uncommment....*/
+// document
+//   .querySelector('.component')
+//   .addEventListener('click', function handleComponentClick(evt){
+//     // YOUR CODE for ADVENTURE MODE
+//
+//   })
